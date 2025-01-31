4664b867bbbf4a186ae9f096bbbef46ce3ff1b38
Limour <93720049+Limour-dev@users.noreply.github.com>
2025-02-01 03:27:09 +0800

    Update app.py

--- launcher/app.py
+++ launcher/app.py
@@ -2,67 +2,69 @@
 
 import os
 from datetime import datetime
-
-from jnius import autoclass, cast
+from kivy.utils import platform
+if platform == "android":
+    from jnius import autoclass, cast
 from kivy.lang import Builder
 from kivy.app import App
-from kivy.utils import platform
-from kivy.properties import ListProperty, BooleanProperty
+from kivy.properties import ListProperty, BooleanProperty, StringProperty
 from os.path import dirname, join, exists
 import traceback
 
-KIVYLAUNCHER_PATHS = os.environ.get("KIVYLAUNCHER_PATHS")
 
-def add_newlines(input_string, _len=50):
-    return [input_string[i:i+_len] for i in range(0, len(input_string), _len)]
+KIVYLAUNCHER_PATHS = os.environ.get("KIVYLAUNCHER_PATHS")
 
 class Launcher(App):
     paths = ListProperty()
-    logs = ListProperty()
+    logs = StringProperty()
     display_logs = BooleanProperty(False)
 
+    def on_request_close(self, *args):
+        import sys
+        sys.exit(0)
+
     def log(self, log):
-        # print(log)
-        log = log.replace('\n', '').replace('\r', '')
-        self.logs.append(f"{datetime.now().strftime('%X.%f')}: ")
-        self.logs.extend(add_newlines(log))
+        self.logs += f"{datetime.now().strftime('%X.%f')}: {log}\n\n"
 
     # try to do this, it will allow you to have access to all directories and files in the storage.
     def permissions_external_storage(self, *args):
         from android import api_version  # 30
         self.log(f'sdk: {api_version}')
 
-        if platform == "android":
+        if api_version > 29:
             PythonActivity = autoclass("org.kivy.android.PythonActivity")
             Environment = autoclass("android.os.Environment")
             Intent = autoclass("android.content.Intent")
             Settings = autoclass("android.provider.Settings")
             Uri = autoclass("android.net.Uri")
-            if api_version > 29:
-                # If you have access to the external storage, do whatever you need
-                if Environment.isExternalStorageManager():
-                    # If you don't have access, launch a new activity to show the user the system's dialog
-                    # to allow access to the external storage
-                    pass
-                else:
-                    try:
-                        activity = PythonActivity.mActivity.getApplicationContext()
-                        uri = Uri.parse("package:" + activity.getPackageName())
-                        intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, uri)
-                        currentActivity = cast(
-                            "android.app.Activity", PythonActivity.mActivity
-                        )
-                        currentActivity.startActivityForResult(intent, 101)
-                    except:
-                        intent = Intent()
-                        intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
-                        currentActivity = cast(
-                            "android.app.Activity", PythonActivity.mActivity
-                        )
-                        currentActivity.startActivityForResult(intent, 101)
+            # If you have access to the external storage, do whatever you need
+            if Environment.isExternalStorageManager():
+                # If you don't have access, launch a new activity to show the user the system's dialog
+                # to allow access to the external storage
+                pass
+            else:
+                try:
+                    activity = PythonActivity.mActivity.getApplicationContext()
+                    uri = Uri.parse("package:" + activity.getPackageName())
+                    intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, uri)
+                    currentActivity = cast(
+                        "android.app.Activity", PythonActivity.mActivity
+                    )
+                    currentActivity.startActivityForResult(intent, 101)
+                except:
+                    intent = Intent()
+                    intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+                    currentActivity = cast(
+                        "android.app.Activity", PythonActivity.mActivity
+                    )
+                    currentActivity.startActivityForResult(intent, 101)
 
     def build(self):
         self.log('start of log')
+
+        if platform != 'android':
+            from kivy.core.window import Window
+            Window.bind(on_request_close=self.on_request_close)
 
         if KIVYLAUNCHER_PATHS:
             self.paths.extend(KIVYLAUNCHER_PATHS.split(","))
@@ -103,6 +105,11 @@ class Launcher(App):
                 "data_entry": entry
             })
         self.root.ids.rv.data = data
+        if data:
+            tl = self.root.ids.tl
+            if tl.parent:
+                tl.parent.remove_widget(tl)
+
 
     def find_entries(self, path=None, paths=None):
         self.log(f'looking for entries in {paths} or {path}')
@@ -132,7 +139,7 @@ class Launcher(App):
         self.log(f'reading entry {filename}')
         data = {}
         try:
-            with open(filename, "r") as fd:
+            with open(filename, "r", encoding='utf-8') as fd:
                 lines = fd.readlines()
                 for line in lines:
                     k, v = line.strip().split("=", 1)
@@ -163,7 +170,7 @@ class Launcher(App):
             os.path.dirname(__file__), "..", "main.py"))
         cmd = Popen([sys.executable, main_py], env=env)
         cmd.communicate()
-        sys.exit(0)
+        # sys.exit(0)
 
     def start_android_activity(self, entry):
         self.log('starting activity')
@@ -187,4 +194,4 @@ class Launcher(App):
         self.log(f'ready to start intent {j_entrypoint} {j_orientation}')
         activity.startActivity(intent)
         self.log('activity started')
-        System.exit(0)
+        # System.exit(0)
